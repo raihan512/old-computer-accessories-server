9844f6af3ecca82c6922176063bdc3b3611541f6
raihan512 <raihangazi1024@Gmail.com>
2022-11-25 14:27:10 +0600

    mongodb database connected to the server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 
 const port = process.env.PORT || 5000;
@@ -15,6 +16,22 @@ app.get('/', (req, res) => {
 })
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e4yec41.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db("pcParts").collection('category');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories)
+        })
+    }
+    finally { }
+}
+run().catch();
 app.listen(port, () => {
     console.log('Server is running');
 })
